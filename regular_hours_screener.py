761ea03524c8becb96f205f6494c512f53fbcb76
ksbhandal <kamal_bhandal@hotmail.com>
2025-06-20 09:53:31 -0700

    regular_hours_screener.py

--- regular_hours_screener.py
+++ regular_hours_screener.py
@@ -56,6 +56,7 @@ def get_metrics(symbol):
         return None
 
 def scan_stocks():
+    print(f"Scanning stocks at {datetime.now()}")  # For debug
     symbols = fetch_stock_symbols()
     matching_stocks = []
 
